80bd0bb760d986f180bdcdb5d7ee4430ab43683c
atytaev <artem.tutaev.2000@gmail.com>
2025-01-13 15:19:43 +0300

    Update

--- task_management/urls.py
+++ task_management/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -14,3 +16,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('<path:path>/', TemplateView.as_view(template_name='index.html')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
